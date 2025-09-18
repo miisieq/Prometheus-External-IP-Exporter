a1deb7cd307191ca0a3b29f18bd879de55725ae7
Michał Szczech <git@miisieq.ovh>
2025-09-18 22:36:04 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,49 +1,58 @@
-from ipaddress import IPv4Address
-from prometheus_client import start_http_server, Gauge, PLATFORM_COLLECTOR, PROCESS_COLLECTOR
-from prometheus_client.core import REGISTRY
-import time
-import requests
-import os
+#!/usr/bin/python3
+# pylint: disable=C0116
+"""
+Simple Prometheus exporter returning external IP v4 address.
+"""
 import ipaddress
-from datetime import datetime
+from fastapi import FastAPI, Response
+from prometheus_client import CollectorRegistry, Gauge, generate_latest
+import requests
+import uvicorn
 
-REFRESH_TIME_SECONDS = os.getenv('REFRESH_TIME_SECONDS', 60)
-REQUEST_TIMEOUT_SECONDS = os.getenv('REQUEST_TIMEOUT_SECONDS', 5)
-HTTP_PORT = os.getenv('HTTP_PORT', 8080)
+app = FastAPI()
+registry = CollectorRegistry()
 
-REGISTRY.unregister(PROCESS_COLLECTOR)
-REGISTRY.unregister(PLATFORM_COLLECTOR)
-REGISTRY.unregister(REGISTRY._names_to_collectors['python_gc_objects_collected_total'])
+metric = Gauge(
+    "external_ip",
+    "Current external IP address",
+    ["ip"],
+    registry=registry,
+)
 
-g = Gauge("external_ip", "Current external IP address", ["ip"])
+@app.get("/")
+async def get_index():
+    content = """
+    <html>
+        <head><title>Prometheus External IP Exporter</title></head>
+        <body>
+            <h1>Prometheus External IP Exporter</h1>
+            <p><a href="metrics">Metrics</a></p>
+            <p><a href="health">Healtcheck</a></p>
+        </body>
+    </html>
+    """
 
-def log_debug(message: str) -> None:
-    current_datetime = datetime.now().isoformat(sep=" ", timespec="seconds")
-    print(f"[{current_datetime}] {message}")
+    return Response(
+        content=content,
+        media_type="text/html; charset=utf-8"
+    )
 
-def get_ip() -> IPv4Address|None:
-    try:
-        response = requests.get("https://ifconfig.co/ip", timeout=REQUEST_TIMEOUT_SECONDS).text.strip()
+@app.get("/health")
+async def get_health():
+    return "I'm okay!"
 
-        try:
-            address = ipaddress.ip_address(response)
+@app.get("/metrics")
+def metrics():
+    response = requests.get("https://ifconfig.co/ip", timeout=5)
+    content = response.text.strip()
+    address = ipaddress.ip_address(content)
+    metric.labels(ip=address).set(1)
 
-            if address.version == 4:
-                log_debug(f"The correct IPv4 address \"{str(address)}\" was returned.")
-                return address
+    return Response(
+        content=generate_latest(registry),
+        media_type="text/plain; version=0.0.4; charset=utf-8",
+    )
 
-        except ValueError:
-            log_debug(f"The returned value \"{response}\" is not a valid IPv4 address.")
-
-        return None
-    except:
-        return None
 
 if __name__ == "__main__":
-    start_http_server(HTTP_PORT)
-    current_ip = None
-    while True:
-        ip = get_ip()
-        if ip:
-            g.labels(ip=ip).set(1)
-        time.sleep(REFRESH_TIME_SECONDS)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
